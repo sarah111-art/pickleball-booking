048dd26c27d86f735bf0666f2b89045a299fa192
sarah111-art <quy073880@gmail.com>
2026-04-13 09:35:11 +0700

    fix(chat): register racket and booking repositories in chat module

--- be-booking/src/modules/chat/chat.module.ts
+++ be-booking/src/modules/chat/chat.module.ts
@@ -5,11 +5,13 @@ import { ChatMessage } from '../../entities/chat-message.entity';
 import { Venue } from '../../entities/venue.entity';
 import { Court } from '../../entities/court.entity';
 import { Location } from '../../entities/location.entity';
+import { Racket } from '../../entities/racket.entity';
+import { Booking } from '../../entities/booking.entity';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatConversation, ChatMessage, Venue, Court, Location])],
+  imports: [TypeOrmModule.forFeature([ChatConversation, ChatMessage, Venue, Court, Location, Racket, Booking])],
   controllers: [ChatController],
   providers: [ChatService],
   exports: [ChatService],
